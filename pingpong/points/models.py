98d69568830d9ff4821e27ff2f3010491f13c352
lollipop1dino <43811294+lollipop1dino@users.noreply.github.com>
2020-01-18 16:11:22 -0500

    Added Models for players and matches

--- pingpong/points/models.py
+++ pingpong/points/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Player(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    email = models.EmailField()
+    rank = models.IntegerField()
+    points = models.IntegerField()
+    
+    def __unicode__(self):
+        return self.name
+
+class Matches(models.Model):
+    players = models.ManyToManyField(Player, related_name='player_playing')
+    winner = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='player_won')
+
+    date = models.DateField()
+    
+    def __unicode__(self):
+        return self.date
+
+
