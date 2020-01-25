4136a726d14d14cd6dc83628554e79d6ed464926
Siddhant Dubey <sid12.dubey@gmail.com>
2020-01-24 21:02:03 -0500

    Fixing heroku

--- pingpong/settings.py
+++ pingpong/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'xtwwh!)q#&rjtk1+(bg25_au284_h%!0_z-yd@&b_&g)-o1ow&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
