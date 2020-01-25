ab6db0c7aaee1d372b1aad5130e25173694df58d
Siddhant Dubey <sid12.dubey@gmail.com>
2020-01-24 19:46:18 -0500

    Continuing error management

--- pingpong/pingpong/settings.py
+++ pingpong/pingpong/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -154,3 +154,4 @@ ACCOUNT_EMAIL_REQUIRED = True
 
 LOGIN_REDIRECT_URL = '/'
 ACCOUNT_LOGOUT_REDIRECT_URL ='/'
+django_heroku.settings(locals())
